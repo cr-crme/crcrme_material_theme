5ffb1d786dc5214479d990d320b0176339c5a6ef
Laurent Stéphenne <guibi.ss999@gmail.com>
2022-06-29 15:10:10 -0400

    Fixed some errors and added an ExpansionPanel

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,6 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<bool> _selected = [true, false];
   int _currentStep = 0;
+  bool expansionPanel = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +24,27 @@ class _HomeState extends State<Home> {
         title: const Text("CR CRME Flutter Template"),
       ),
       drawer: Drawer(
-          child: Column(
-        children: [
-          ListTile(
-            title: const Text("Button Showcase"),
-            onTap: () => Navigator.pushNamed(context, ButtonShowcase.routeName),
-          ),
-          ListTile(
-            title: const Text("Input Showcase"),
-            onTap: () => Navigator.pushNamed(context, InputShowcase.routeName),
-          ),
-          ListTile(
-            title: const Text("Dialog Showcase"),
-            onTap: () => Navigator.pushNamed(context, DialogShowcase.routeName),
-          ),
-        ],
-      )),
+          child: Scaffold(
+              appBar: AppBar(),
+              body: Column(
+                children: [
+                  ListTile(
+                    title: const Text("Button Showcase"),
+                    onTap: () =>
+                        Navigator.pushNamed(context, ButtonShowcase.routeName),
+                  ),
+                  ListTile(
+                    title: const Text("Input Showcase"),
+                    onTap: () =>
+                        Navigator.pushNamed(context, InputShowcase.routeName),
+                  ),
+                  ListTile(
+                    title: const Text("Dialog Showcase"),
+                    onTap: () =>
+                        Navigator.pushNamed(context, DialogShowcase.routeName),
+                  ),
+                ],
+              ))),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(8.0),
         child: Center(
@@ -48,80 +54,104 @@ class _HomeState extends State<Home> {
                 "Voici une application qui montre tous (presque) les widgets Flutter ainsi que leur theme !"),
             const Text("Naviguez via le Drawer en haut à gauche !"),
             Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(8.0),
+              child: ExpansionPanelList(
+                  expansionCallback: (int panelIndex, bool isExpanded) {
+                    setState(() {
+                      expansionPanel = !isExpanded;
+                    });
+                  },
+                  children: [
+                    ExpansionPanel(
+                        isExpanded: expansionPanel,
+                        canTapOnHeader: true,
+                        headerBuilder: (BuildContext context, bool open) =>
+                            const Text("Expansion Panel List"),
+                        body: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Chip(label: Text("Chip 1")),
+                            Chip(label: Text("Chip 2")),
+                            Chip(label: Text("Chip 3"))
+                          ],
+                        ))
+                  ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Card(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
-                    children: [
-                      const Padding(
+                    children: const [
+                      Padding(
                         padding: EdgeInsets.all(8.0),
                         child: Text("Card"),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Chip(label: Text("Chip 1")),
-                          Chip(label: Text("Chip 2")),
-                          Chip(label: Text("Chip 3"))
-                        ],
-                      ),
-                      const Divider(),
-                      const Padding(
+                      Divider(),
+                      Padding(
                         padding: EdgeInsets.all(8.0),
                         child: CircularProgressIndicator(),
                       ),
-                      const LinearProgressIndicator(
+                      LinearProgressIndicator(
                         value: null,
                       ),
-                      const Divider(),
-                      DataTable(
-                          onSelectAll: (value) {
-                            setState(() {
-                              _selected = [value!, value];
-                            });
-                          },
-                          columns: const [
-                            DataColumn(label: Text("Column 1")),
-                            DataColumn(label: Text("Column 2")),
-                            DataColumn(label: Text("Column 3"))
-                          ],
-                          rows: [
-                            DataRow(
-                                selected: _selected[0],
-                                onSelectChanged: (bool? v) {
-                                  setState(() {
-                                    _selected[0] = v!;
-                                  });
-                                },
-                                cells: const [
-                                  DataCell(Text("Cell 1-1")),
-                                  DataCell(Text("Cell 1-2")),
-                                  DataCell(Text("Cell 1-3"))
-                                ]),
-                            DataRow(
-                                selected: _selected[1],
-                                onSelectChanged: (bool? v) {
-                                  setState(() {
-                                    _selected[1] = v!;
-                                  });
-                                },
-                                cells: const [
-                                  DataCell(Text("Cell 2-1")),
-                                  DataCell(Text("Cell 2-2")),
-                                  DataCell(Text("Cell 2-3"))
-                                ])
-                          ])
                     ],
                   ),
                 ),
               ),
             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                  onSelectAll: (value) {
+                    setState(() {
+                      _selected = [value!, value];
+                    });
+                  },
+                  columns: const [
+                    DataColumn(label: Text("Column 1")),
+                    DataColumn(label: Text("Column 2")),
+                    DataColumn(label: Text("Column 3")),
+                    DataColumn(label: Text("Column 4")),
+                    DataColumn(label: Text("Column 5"))
+                  ],
+                  rows: [
+                    DataRow(
+                        selected: _selected[0],
+                        onSelectChanged: (bool? v) {
+                          setState(() {
+                            _selected[0] = v!;
+                          });
+                        },
+                        cells: const [
+                          DataCell(Text("Cell 1-1")),
+                          DataCell(Text("Cell 1-2")),
+                          DataCell(Text("Cell 1-3")),
+                          DataCell(Text("Cell 1-4")),
+                          DataCell(Text("Cell 1-5"))
+                        ]),
+                    DataRow(
+                        selected: _selected[1],
+                        onSelectChanged: (bool? v) {
+                          setState(() {
+                            _selected[1] = v!;
+                          });
+                        },
+                        cells: const [
+                          DataCell(Text("Cell 2-1")),
+                          DataCell(Text("Cell 2-2")),
+                          DataCell(Text("Cell 2-3")),
+                          DataCell(Text("Cell 2-4")),
+                          DataCell(Text("Cell 2-5"))
+                        ])
+                  ]),
+            ),
             Stepper(
                 currentStep: _currentStep,
                 onStepContinue: () {
                   setState(() {
-                    _currentStep += 1;
+                    _currentStep = (_currentStep + 1) % 3;
                   });
                 },
                 onStepCancel: () {
