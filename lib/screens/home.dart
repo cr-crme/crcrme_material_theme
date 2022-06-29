f1ac1c3476e8a5fc41c081387d46b9f3c99a7c27
Laurent Stéphenne <guibi.ss999@gmail.com>
2022-06-29 12:04:55 -0400

    Added scroll to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
           ),
         ],
       )),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(8.0),
         child: Center(
             child: Column(
