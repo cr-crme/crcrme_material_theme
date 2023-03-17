96079a23ba82cb02821379542e7f8397b92e517c
Pariterre <pariterre@hotmail.com>
2023-03-17 08:04:56 -0400

    Changed " for '

--- example/screens/dialog_showcase.dart
+++ example/screens/dialog_showcase.dart
@@ -8,7 +8,7 @@ class DialogShowcase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Dialog Showcase")),
+      appBar: AppBar(title: const Text('Dialog Showcase')),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -20,17 +20,17 @@ class DialogShowcase extends StatelessWidget {
                   showDialog(
                       context: context,
                       builder: (BuildContext context) => AlertDialog(
-                            title: const Text("Alert Dialog"),
+                            title: const Text('Alert Dialog'),
                             content: const Text(
-                                "Consequatur laudantium nisi odio assumenda. Omnis et nobis ullam impedit. Deleniti ut rem cupiditate qui est porro neque. Deleniti unde deserunt sequi ut doloremque. Et molestiae eos consequatur deleniti eos aut sapiente nesciunt."),
+                                'Consequatur laudantium nisi odio assumenda. Omnis et nobis ullam impedit. Deleniti ut rem cupiditate qui est porro neque. Deleniti unde deserunt sequi ut doloremque. Et molestiae eos consequatur deleniti eos aut sapiente nesciunt.'),
                             actions: [
                               TextButton(
-                                  onPressed: () => Navigator.pop(context, "OK"),
-                                  child: const Text("OK"))
+                                  onPressed: () => Navigator.pop(context, 'OK'),
+                                  child: const Text('OK'))
                             ],
                           ));
                 },
-                child: const Text("Alert Dialog")),
+                child: const Text('Alert Dialog')),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -43,10 +43,10 @@ class DialogShowcase extends StatelessWidget {
                           builder: (BuildContext context) => const Padding(
                                 padding: EdgeInsets.all(30.0),
                                 child: Text(
-                                    "Rerum porro ut modi atque cupiditate. Voluptatibus adipisci id veritatis dolore sed placeat molestiae. Officiis assumenda pariatur sit."),
+                                    'Rerum porro ut modi atque cupiditate. Voluptatibus adipisci id veritatis dolore sed placeat molestiae. Officiis assumenda pariatur sit.'),
                               )));
                 },
-                child: const Text("Bottom Sheet")),
+                child: const Text('Bottom Sheet')),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -54,7 +54,7 @@ class DialogShowcase extends StatelessWidget {
                 onPressed: () {
                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                       action: SnackBarAction(
-                        label: "Annuler",
+                        label: 'Annuler',
                         onPressed: () {},
                       ),
                       content: Row(
@@ -63,11 +63,11 @@ class DialogShowcase extends StatelessWidget {
                             padding: EdgeInsets.all(8.0),
                             child: Icon(Icons.check),
                           ),
-                          Text("Enregistré"),
+                          Text('Enregistré'),
                         ],
                       )));
                 },
-                child: const Text("Snack Bar")),
+                child: const Text('Snack Bar')),
           ),
         ],
       )),
