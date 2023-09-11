504839af40cbe44a157890fd15dd88a76830674a
Pariterre <benjamin.michaud.phq@gmail.com>
2023-09-11 09:54:06 -0400

    Fixed bug introduced with newest version of flutter

--- example/screens/home.dart
+++ example/screens/home.dart
@@ -67,7 +67,7 @@ class _HomeState extends State<Home> {
               child: ExpansionPanelList(
                   expansionCallback: (int panelIndex, bool isExpanded) {
                     setState(() {
-                      expansionPanel = !isExpanded;
+                      expansionPanel = isExpanded;
                     });
                   },
                   children: [
